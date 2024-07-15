a3ab336c089a95d635ca45a7c0214185ec529ef8
PGJ0209 <geonjun1401@naver.com>
2024-07-15 17:22:56 +0900

    0715_수정

--- MYSQL/server.js
+++ MYSQL/server.js
@@ -1,13 +1,45 @@
 const express = require("express");
-const mysql = require("musql");
+const mysql = require("mysql");
 const app = express();
 const port = 3000;
 
-app.get("/", (res, req) => {
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "pgj",
+  port: 3306,
+  password: "1234",
+  database: "test_db",
+});
+
+db.connect((error) => {
+  if (error) {
+    console.log("접속실패!!");
+    return;
+  }
+  console.log("MySQL Connected!");
+});
+
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
   console.log("웹에 정상 접속 하였습니다.");
 });
 
+app.get("/data", (req, res) => {
+  const { name, dob } = req.query;
+  db.query(
+    "INSERT INTO web (name, dob) VALUES (?,?)",
+    [name, dob],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log(`name: ${name}, dob: ${dob}`);
+      console.log("Data inserted successfully");
+    }
+  ); // MySQL query here
+});
+
 app.listen(port, () => {
-  console.log("Server is running at http://localhost:%(port)");
+  console.log(`Server is running at http://localhost:${port}`);
 });
