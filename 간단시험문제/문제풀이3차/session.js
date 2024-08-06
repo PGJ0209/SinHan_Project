a9a804619511bb4c8c5e55f2a0880b51cec9807c
PGJ0209 <geonjun1401@naver.com>
2024-08-06 15:31:49 +0900

    0806

--- 간단시험문제/문제풀이3차/session.js
+++ 간단시험문제/문제풀이3차/session.js
@@ -15,30 +15,46 @@ app.use(
 
 app.get("/", (req, res) => {
   if (req.session.loggedIn) {
-    res.send(`
-        <h2>${req.session.username}님 안녕하세요</h2>
-        <h2>${req.session.username}님의 개인공간입니다.</h2>
-        <hr/>
-        <h2>대충 개인 데이터베이스 목록</h2>
-        <button onclick="location.href='/logout'">로그아웃</button>
-        `);
+    res.sendFile(__dirname + "/ToDoList.html");
   } else {
-    res.sendFile(__dirname + "/index.html");
+    res.send(` 
+    <button onclick="window.location.href='/join'">회원가입</button>
+    <button onclick="window.location.href='/login'">로그인</button>
+    <hr />
+    <h2>할 일 목록 (비회원 입니다.)</h2>
+    <input type="text" />
+    <button onclick="alert('로그인이 필요합니다.');window.location.href='/login'">추가하기</button>
+    <div></div>`);
   }
+});
+
+app.get("/join", (req, res) => {
+  res.sendFile(__dirname + "/join.html");
 });
 
 app.get("/login", (req, res) => {
   res.sendFile(__dirname + "/login.html");
 });
 
+// logout
+app.get("/logout", (req, res) => {
+  req.session.loggedIn = false;
+  req.session.adminOk = false;
+  res.send(
+    `<script>alert('로그아웃 되었습니다.');window.location.href='/'</script>`
+  );
+});
+
+// login 유효성 검사 란
 app.post("/login", (req, res) => {
   const { username, password } = req.body; // query 는 get 방식
   const idOK = /^[A-Za-z0-9]{1,8}$/g.test(username); // 방법1. true or false 반환
   const pwOK = password.match(/^[A-Za-z0-9]{1,8}$/g); // 방법2. 정규표현식에 일치한 값
   console.log(idOK, pwOK, !!pwOK);
 
+  // login 로그인 조건
   if (idOK && !!pwOK) {
-    if (username === "test" && password == "1234") {
+    if (username === "user" && password == "1234") {
       req.session.loggedIn = true;
       req.session.username = username;
       res.redirect("/");
@@ -46,6 +62,7 @@ app.post("/login", (req, res) => {
       res.send(`
         <h3>정상적인 로그인이 필요합니다.</h3>
         <button onclick="location.href='/'">뒤로가기</button>
+        <button onclick="location.href='/join'">가입하기</button>
         `);
     }
   } else {
@@ -59,7 +76,9 @@ app.post("/login", (req, res) => {
 app.get("/logout", (req, res) => {
   req.session.destroy((e) => {
     if (e) console.error(e);
-    res.send(`<script>alert('로그아웃이 되었습니다!!');window.location.href='/'</script>`);
+    res.send(
+      `<script>alert('로그아웃이 되었습니다!!');window.location.href='/'</script>`
+    );
   });
 });
 
