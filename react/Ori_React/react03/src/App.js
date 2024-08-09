835e846b36c20f848133d2ac7b0958996fd3636a
PGJ0209 <geonjun1401@naver.com>
2024-08-09 17:14:28 +0900

    0809_오후커밋

--- react/Ori_React/react03/src/App.js
+++ react/Ori_React/react03/src/App.js
@@ -1,20 +1,22 @@
-import "./App.css";
-import { useState } from "react";
-import Join from "./pages/join";
+import './App.css';
+import { useState } from 'react';
+import Join from './pages/join.jsx';
 function App() {
-  const [text, setText] = useState("안녕하세요.");
+    const [text, setText] = useState('');
+    const handleInput = (e) => setText(e.target.value);
 
-  const handleInput = (e) => setText(e.target.value);
-  const handleDelete = (e) => setText("");
-  return (
-    <>
-      <div>리액트 시작 03</div>
-      <input type="text" onChange={handleInput} value={text} />
-      <button onClick={handleDelete}>지우기</button>
-      <h3>{text}</h3>
-      <Join></Join>
-    </>
-  );
+    return (
+        <div>
+          <Join />
+            {/* <form>
+                <div>리액트 시작 03</div>
+                <input type="text" onChange={handleInput} />
+                <h3>{text}</h3>
+                <button type="delete">지우기</button>
+                <button type="reset">지우기</button>
+            </form> */}
+        </div>
+    );
 }
 
 export default App;
